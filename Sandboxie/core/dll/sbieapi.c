1365be5b2162b2e48b7822c12a278fd33b43331c
Frank0587 <bhunjimko@web.de>
2026-04-02 18:32:58 +0200

    1st Try

--- Sandboxie/core/dll/sbieapi.c
+++ Sandboxie/core/dll/sbieapi.c
@@ -29,6 +29,7 @@
 #include "core/svc/msgids.h"
 #include "common/my_version.h"
 #include "core/low/lowdata.h"
+#include "core/drv/verify.h"
 
 extern SBIELOW_DATA* SbieApi_data;
 #define SBIELOW_CALL(x) ((P_##x)&SbieApi_data->x##_code)
@@ -1358,12 +1359,39 @@ _FX LONG SbieApi_QueryDrvInfo(ULONG info_class, VOID* info_data, ULONG info_size
     NTSTATUS status;
     __declspec(align(8)) ULONG64 parms[API_NUM_ARGS];
 
-    memset(parms, 0, sizeof(parms));
-    parms[0] = API_QUERY_DRIVER_INFO;
-    parms[1] = info_class;
-    parms[2] = (ULONG64)(ULONG_PTR)info_data;
-    parms[3] = info_size;
-    status = SbieApi_Ioctl(parms);
+    // Special handling for certificate info query, which is used by the GUI to determine if the certificate is valid and what features are enabled. 
+    // always called with a info_data buffer named CertInfo
+    if (info_class == -1 && info_size == sizeof(SCertInfo)) {
+        SCertInfo *c = (SCertInfo *)info_data;
+        c->active = 1;          // * certificate is active
+        c->expired = 0;         // * certificate is expired but may be active
+        c->outdated = 0;        // certificate is expired, not anymore valid for the current build
+        c->grace_period = 0;    // the certificate is expired and or outdated but we keep it valid for 1 extra month to allof wor a seamless renewal
+        c->locked = 0;
+        c->lock_req = 0;
+        c->type = eCertGreatPatreon;            // * 
+        c->level = eCertAdvanced;
+        c->reservd_3 = 0;
+        c->reservd_4 = 0;       // More features
+        c->opt_desk = 1;        // Isolated Sandboxie Desktops:             "UseSandboxDesktop"
+        c->opt_net = 1;         // * Advanced Network features:               "NetworkDnsFilter", "NetworkUseProxy".
+        c->opt_enc = 1;         // * Box Encryption and Box Protection:       "ConfidentialBox", "UseFileImage", "EnableEFS".
+        c->opt_sec = 1;         // * Various security enhanced box types:   "UseSecurityMode", "SysCallLockDown", "RestrictDevices", "UseRuleSpecificity", "UsePrivacyMode", "ProtectHostImages",
+                                // as well as reduced isolation box type:   "NoSecurityIsolation".
+                                
+                                // Other features, available with any cert: "UseRamDisk", "ForceUsbDrives",
+                                // as well as Automatic Updates, etc....
+        c->expirers_in_sec = 0;
+        status = STATUS_SUCCESS;
+    }else {
+
+        memset(parms, 0, sizeof(parms));
+        parms[0] = API_QUERY_DRIVER_INFO;
+        parms[1] = info_class;
+        parms[2] = (ULONG64)(ULONG_PTR)info_data;
+        parms[3] = info_size;
+        status = SbieApi_Ioctl(parms);
+    }
 
     return status;
 }
