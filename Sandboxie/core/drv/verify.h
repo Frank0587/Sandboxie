62247399d88ca1f98bc7b19c53705d926179718f
Frank0587 <bhunjimko@web.de>
2026-04-03 00:19:45 +0200

    4th Try

--- Sandboxie/core/drv/verify.h
+++ Sandboxie/core/drv/verify.h
@@ -96,21 +96,21 @@ enum ECertLevel {
 
 #define FillCertInfo(c)  \
     do {   \
-        c->active = 1;               \
-        c->expired = 0;              \
-        c->outdated = 0;             \
-        c->grace_period = 0;         \
-        c->locked = 0;               \
-        c->lock_req = 0;             \
-        c->type = eCertGreatPatreon; \
-        c->level = eCertAdvanced;    \
-        c->reservd_3 = 0;            \
-        c->reservd_4 = 0;            \
-        c->opt_desk = 1;             \
-        c->opt_net = 1;              \
-        c->opt_enc = 1;              \
-        c->opt_sec = 1;              \
-        c->expirers_in_sec = 0;      \
+        (c)->active = 1;               \
+        (c)->expired = 0;              \
+        (c)->outdated = 0;             \
+        (c)->grace_period = 0;         \
+        (c)->locked = 0;               \
+        (c)->lock_req = 0;             \
+        (c)->type = eCertGreatPatreon; \
+        (c)->level = eCertAdvanced;    \
+        (c)->reservd_3 = 0;            \
+        (c)->reservd_4 = 0;            \
+        (c)->opt_desk = 1;             \
+        (c)->opt_net = 1;              \
+        (c)->opt_enc = 1;              \
+        (c)->opt_sec = 1;              \
+        (c)->expirers_in_sec = 0;      \
     } while(0) 
 
 
